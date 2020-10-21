95155f3b27081b3ad0dc4abc5cf60447ab70a0f6
miquelarrom1908 <miquelarrom@paucasesnovescifp.cat>
2020-10-21 18:25:05 +0200

    In the first exercise I made two lists, one adding names to the list and then with a loop I wrote them on the screen. In the second list I created a class, that is, an object, to be able to add objects in the list, and with a loop I wrote them on the screen.

---
+++ ConsoleAppExerciceForeachAndLists/PersonModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExerciceForeachAndLists
+{
+    public class PersonModel
+    {
+        
+        private String firstName;
+        private String lastName;
+
+        public PersonModel(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+       
+
+    }
+}
---
+++ ConsoleAppExerciceForeachAndLists/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExerciceForeachAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.list1();
+            Console.WriteLine("");
+            p.list2();
+        }
+
+        public void list1()
+        {
+            List<String> names = new List<String>();
+
+            names.Add("John");
+            names.Add("Mary");
+            names.Add("Sue");
+            names.Add("Greg");
+            names.Add("Yolanda");
+            names.Add("Jose");
+            names.Add("Bill");
+
+            foreach (String x in names)
+            {
+                Console.WriteLine($"Hello {x}");
+            }
+            Console.ReadKey();
+        }
+
+        public void list2()
+        {
+            List<PersonModel> names = new List<PersonModel>();
+
+            names.Add(new PersonModel("Tim", "Corey"));
+            names.Add(new PersonModel("Bill", "McCoy"));
+            names.Add(new PersonModel("Mary", "Jones"));
+            names.Add(new PersonModel("Sue", "Smith"));
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.FirstName} {name.LastName}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
